602a4f6ce682428153237715c0637b1f174e3e96
Kristian Poslek <kristian.poslek@comsysto.com>
2015-08-08 17:18:57 +0200

    Tray icon and menu working

--- app/js/index.js
+++ app/js/index.js
@@ -35,3 +35,38 @@ var settingsEl = document.querySelector('.settings');
 settingsEl.addEventListener('click', function () {
     ipc.send('open-settings-window');
 });
+
+var remote = require('remote');
+var Tray = remote.require('tray');
+var Menu = remote.require('menu');
+var path = require('path');
+
+var trayIcon = null;
+
+if (process.platform === 'darwin') {
+    trayIcon = new Tray(path.join(__dirname, 'img/tray-iconTemplate.png'));
+}
+else {
+    trayIcon = new Tray(path.join(__dirname, 'img/tray-icon-alt.png'));
+}
+
+var trayMenuTemplate = [
+    {
+        label: 'Sound machine',
+        enabled: false
+    },
+    {
+        label: 'Settings',
+        click: function () {
+            ipc.send('open-settings-window');
+        }
+    },
+    {
+        label: 'Quit',
+        click: function () {
+            ipc.send('close-main-window');
+        }
+    }
+];
+var trayMenu = Menu.buildFromTemplate(trayMenuTemplate);
+trayIcon.setContextMenu(trayMenu);
